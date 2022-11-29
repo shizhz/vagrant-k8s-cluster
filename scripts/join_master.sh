1d6ac8800511f90755352646460f378da21713b7
Shizhz <messi.shizz@gmail.com>
2022-11-29 22:30:01 +0800

    remove unused scripts

--- scripts/join_master.sh
+++
@@ -1,1 +0,0 @@
-kubeadm join k8s.shizhz.me:6443 --token sg70wz.vl1xwsd4ht6hj0cd --discovery-token-ca-cert-hash sha256:16214b97fcef64c644edb484c578d37f77bfdd5f180bc94dcc6fe36962e733f7 --control-plane --certificate-key 4c7f5786ac967bea742d1f2c95ee0f9055f0fd6258a1971f1eb79fbc1e173943 $@
--- scripts/join_worker.sh
+++
@@ -1,1 +0,0 @@
-kubeadm join k8s.shizhz.me:6443 --token za2x7t.na2opufo6vuvrbip --discovery-token-ca-cert-hash sha256:16214b97fcef64c644edb484c578d37f77bfdd5f180bc94dcc6fe36962e733f7
